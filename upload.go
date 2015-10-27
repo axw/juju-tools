38861036a04324ad2a80e2c58c0ee82cd1d1f872
Andrew Wilkins <axwalk@gmail.com>
2015-10-27 18:34:00 +0800

    Implement upload command

--- upload.go
+++ upload.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/juju/cmd"
+	"github.com/juju/errors"
 	"github.com/juju/juju/cmd/envcmd"
+	"github.com/juju/juju/version"
 	"launchpad.net/gnuflag"
 )
 
@@ -11,13 +17,12 @@ const uploadToolsCommandDoc = `
 Juju tools upload is used to upload tools to the Juju controller.
 `
 
+const toolsPrefix = "juju-"
+const toolsSuffix = ".tgz"
+
 type uploadToolsCommand struct {
 	envcmd.EnvCommandBase
-}
-
-// Init implements Command.Init.
-func (c *uploadToolsCommand) Init(args []string) (err error) {
-	return nil
+	archives []string
 }
 
 // Info implements Command.Info.
@@ -34,7 +39,49 @@ func (c *uploadToolsCommand) SetFlags(f *gnuflag.FlagSet) {
 	c.EnvCommandBase.SetFlags(f)
 }
 
+// Init implements Command.Init.
+func (c *uploadToolsCommand) Init(args []string) error {
+	if len(args) == 0 {
+		return errors.New("specify one or more tools archives to upload")
+	}
+	c.archives = args
+	return nil
+}
+
 // Run implements Command.Run.
 func (c *uploadToolsCommand) Run(ctx *cmd.Context) error {
+	client, err := c.NewAPIClient()
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	versions := make([]version.Binary, len(c.archives))
+	for i, archive := range c.archives {
+		basename := filepath.Base(archive)
+		if !strings.HasPrefix(basename, toolsPrefix) || !strings.HasSuffix(basename, toolsSuffix) {
+			return errors.NotValidf("tools archive %q", archive)
+		}
+		versionString := basename[len(toolsPrefix) : len(basename)-len(toolsSuffix)]
+		binary, err := version.ParseBinary(versionString)
+		if err != nil {
+			return errors.NotValidf("tools archive %q", archive)
+		}
+		versions[i] = binary
+	}
+
+	for i, archive := range c.archives {
+		ctx.Infof("uploading %q", archive)
+		r, err := os.Open(archive)
+		if err != nil {
+			return err
+		}
+		_, err = client.UploadTools(r, versions[i])
+		r.Close()
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
